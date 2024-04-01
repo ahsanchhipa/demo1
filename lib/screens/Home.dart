3c09d25079631a916180540045368d66bc4d11f8
ahsan.chhipa <mahsan.hashtag@gmail.com>
2024-04-01 15:41:21 +0500

    last commit

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,34 +1,29 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+   Home({super.key});
+
+
+
+  List<String> products = ["Bed","Sofa","Chair"];
+  List<String> productsDetails = ["King Size Bed","King Size Sofa","Wooden Chair"];
+  List<int> price = [3000, 6000, 1000];
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      body: Center(
-        child: ElevatedButton.icon(
+      appBar: AppBar(title: Text("Home",style: TextStyle(color: Colors.white)),centerTitle: true,
+          backgroundColor: Colors.purple,
+        leading: IconButton(icon: Icon(Icons.menu),onPressed: (){},color: Colors.white,),),
 
-          // child: Text("Submit"),
-          icon: Icon(Icons.account_circle_outlined),
-          label: Text("Login"),
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-            foregroundColor: Colors.black,
-            backgroundColor: Colors.yellow,
-            padding: EdgeInsets.all(20.0),
-            fixedSize: Size(200.0, 80.0),
-            textStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-            elevation: 15,
-            shadowColor: Colors.orange,
-              side: BorderSide(color: Colors.black,width: 5.0),
-            // alignment: Alignment.topLeft,
-            shape: LinearBorder()
+      body: Container(
+        child: ListView.builder(
+          itemCount: products.length,
+          itemBuilder: (context,index){
+            return Text(products[index]);
+          },
 
-
-
-          ),
         ),
       ),
     );
